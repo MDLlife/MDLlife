757ee8b7c02070bcad0b68bff81566e617c1182c
Thomas Modeneis <thomas.modeneis@gmail.com>
2018-11-10 23:17:37 +0100

    updating blog

--- images/reduce.sh
+++ images/reduce.sh
@@ -9,6 +9,6 @@ for i in $(find . -type f -name '201*.*g' |sort); do
   let counter++
   if [[ "$counter" -gt 0 ]]; then
     echo $i
-    convert -resize 375x216 -quality $quality $PWD/$i "$PWD/$quality/$i"
+    convert -resize 600x400 -quality $quality $PWD/$i "$PWD/$quality/$i"
   fi
 done
